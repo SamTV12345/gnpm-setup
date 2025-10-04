6ba72c461f65918332205fc1a016de572a692429
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-10-04 23:16:52 +0200

    chore: use node for tempdir

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {setFailed, saveState, getInput} from '@actions/core'
 import {createReadStream, existsSync, symlinkSync, writeFileSync, copyFileSync} from 'node:fs'
 import tar from 'tar-fs'
+import {tmpdir} from "node:os";
+import {join} from "node:path";
 const gnpmPath = '/usr/local/bin/gnpm';
 import { createGunzip } from 'node:zlib';
 
@@ -11,8 +13,8 @@ const main = async () => {
     const arch = process.arch === 'x64' ? 'amd64' : process.arch;
     const platform = process.platform === 'win32' ? 'windows' : process.platform;
     const link = `https://github.com/SamTV12345/gnpm/releases/download/v${version}/gnpm_${version}_${platform}_${arch}.tar.gz`
-    const tarFileLocation = "/tmp/gnpm.tar.gz"
-    const tarFileTargetLocation = "/tmp/gnpmTarget"
+    const tarFileLocation = join(tmpdir(), "gnpm.tar.gz")
+    const tarFileTargetLocation = join(tmpdir(),"gnpmTarget")
     const actualInstallPath = gnpmPath + `-${version}`
 
     if (existsSync(actualInstallPath)) {
@@ -29,14 +31,14 @@ const main = async () => {
             const arrayBuffer = await gnpmBinary.arrayBuffer();
             const buffer = Buffer.from(arrayBuffer);
             saveState('gnpmPath', gnpmPath);
-            writeFileSync("/tmp/gnpm.tar.gz", buffer, {
+            writeFileSync(tarFileLocation, buffer, {
                 mode: 0o755
             })
 
            createReadStream(tarFileLocation).pipe(createGunzip()).pipe(tar.extract(tarFileTargetLocation))
                .on('finish', ()=>{
                    console.log('Finished gnpm binary done.')
-                   copyFileSync(`${tarFileTargetLocation}/gnpm`, actualInstallPath);
+                   copyFileSync(join(tarFileTargetLocation, 'gnpm'), actualInstallPath);
                    try {
                        symlinkSync(actualInstallPath, gnpmPath)
                        console.log(`Successfully installed gnpm version ${version}.`)
